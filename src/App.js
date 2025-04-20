593c6a503e94d435995c5385a69e27c8ba3a2413
Igor_Maer <igor.son303@yandex.by>
2025-04-20 22:47:39 +0300

    home

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <Star/>
+  );
+}
+
+function Star () {
+return (
+  <div>
+    <h1>Super Star</h1>
+  </div>
+);
+}
+
+export default App;
